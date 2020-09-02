299c5af55648f1e3df0a7af54ceb649dad953903
Cameron Clark <sparx.cam@gmail.com>
2020-09-02 16:50:06 -0400

    del 1 step 60

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,11 +1,18 @@
 var controllerOptions = {};
-var i = 0;
 var x = window.innerWidth / 2;
 var y = window.innerHeight / 2;
+var hand;
 Leap.loop(controllerOptions, function(frame)
 {
-	console.log(i)
-	i++;
-	circle(x,y,50)
+	//clear();
+	//x = x + Math.floor(Math.random() * 3) - 1;
+	//y = y + Math.floor(Math.random() * 3) - 1;
+	//circle(x,y,50)
+	
+	if (frame.hands.length == 1) {
+		hand = frame.hands[0];
+		fingers = hand.fingers
+		console.log(hand.fingers)
+	}
 }
 );
