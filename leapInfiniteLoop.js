777d0b71116ad5ce8fa0f67219a15428b9e30713
Cameron Clark <sparx.cam@gmail.com>
2020-09-02 16:30:33 -0400

    del 1 step 48

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,7 +1,11 @@
 var controllerOptions = {};
 var i = 0;
-Leap.loop(controllerOPtions, function(frame)
+var x = window.innerWidth / 2;
+var y = window.innerHeight / 2;
+Leap.loop(controllerOptions, function(frame)
 {
 	console.log(i)
+	i++;
+	circle(x,y,50)
 }
 );
